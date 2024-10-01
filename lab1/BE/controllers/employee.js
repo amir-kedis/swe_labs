9ad77ebce91a8a62102947bba81433906c578c67
amir-kedis <amir.kedis@gmail.com>
2024-10-01 16:50:12 +0300

    feat: implement lab 1

--- lab1/BE/controllers/employee.js
+++ lab1/BE/controllers/employee.js
@@ -1,15 +1,59 @@
-const employee = [
-  { id: '1', name: 'Mohamed Sayed' },
-];
+const employee = [{ id: "1", name: "Amir Kedis" }];
 
-exports.getEmployees = async (req, res, next) => {
-  res.status(200).json({ data: employee });
+const RESPONSE_CDOE = {
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  CREATED: 201,
+  OK: 200,
+};
+
+exports.getEmployees = async (_req, res, _next) => {
+  res.status(RESPONSE_CDOE.OK).json({ data: employee });
 };
 
 // TODO
-exports.deleteEmployee = async (req, res, next) => {
+exports.deleteEmployee = async (req, res, _next) => {
+  const { id } = req.params;
+  const employeeToBeDeleted = employee.find((emp) => emp.id === id);
+
+  if (!employeeToBeDeleted) {
+    return res
+      .status(RESPONSE_CDOE.NOT_FOUND)
+      .json({ message: "Employee not found" });
+  }
+
+  const index = employee.indexOf(employeeToBeDeleted);
+  employee.splice(index, 1);
+
+  res.status(RESPONSE_CDOE.OK).json({
+    data: {
+      deletedEmployee: employeeToBeDeleted,
+    },
+  });
 };
 
 // TODO
-exports.createEmployee = async (req, res, next) => {
+exports.createEmployee = async (req, res, _next) => {
+  const { id, name } = req.body;
+
+  if (!id || !name) {
+    return res
+      .status(RESPONSE_CDOE.BAD_REQUEST)
+      .json({ message: "Bad request" });
+  }
+
+  if (employee.find((emp) => emp.id === id)) {
+    return res
+      .status(RESPONSE_CDOE.BAD_REQUEST)
+      .json({ message: "Employee already exists" });
+  }
+
+  const newEmployee = { id, name };
+  employee.push(newEmployee);
+
+  res.status(RESPONSE_CDOE.CREATED).json({
+    data: {
+      newEmployee,
+    },
+  });
 };
--- lab1/FE/index.js
+++ lab1/FE/index.js
@@ -1,52 +1,93 @@
 function fetchEmployees() {
-  fetch('http://localhost:3000/api/v1/employee')
-    .then(response => response.json())
-    .then(data => {
-      const tableBody = document.getElementById('dataTable')
-      tableBody.innerHTML = ''
-      const list = data.data
-      list.forEach(item => {
-        const row = document.createElement('tr')
-        const idCell = document.createElement('td')
-        idCell.textContent = item.id
-        row.appendChild(idCell)
+  fetch("http://localhost:3000/api/v1/employee")
+    .then((response) => response.json())
+    .then((data) => {
+      const tableBody = document.getElementById("dataTable");
+      tableBody.innerHTML = "";
+      const list = data.data;
+      list.forEach((item) => {
+        const row = document.createElement("tr");
+        const idCell = document.createElement("td");
+        idCell.dataset.id_cell = true;
+        idCell.textContent = item.id;
+        row.appendChild(idCell);
 
-        const nameCell = document.createElement('td')
-        nameCell.textContent = item.name
-        row.appendChild(nameCell)
+        const nameCell = document.createElement("td");
+        nameCell.textContent = item.name;
+        row.appendChild(nameCell);
 
-        const deleteCell = document.createElement('td')
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.classList.add('btn', 'btn-danger', 'btn-sm');
+        const deleteCell = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("btn", "btn-danger", "btn-sm");
         deleteCell.appendChild(deleteButton);
 
-        row.appendChild(deleteCell)
+        row.appendChild(deleteCell);
 
-        tableBody.appendChild(row)
-      })
+        tableBody.appendChild(row);
+      });
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+}
+
+const submitButton = document.querySelector("button[type='submit']");
+
+// TODO
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  createEmployee();
+});
+
+// TODO
+// NOTE: this way is better than cluttering the event loop with so many event listeners
+document.addEventListener("click", (e) => {
+  if (e.target && e.target.className.includes("btn-danger")) {
+    const id =
+      e.target.parentElement.parentElement.querySelector("[data-id_cell]");
+    deleteEmployee(id?.textContent);
+  }
+});
+
+// TODO
+async function createEmployee() {
+  const name = document.getElementById("name").value;
+  const id = document.getElementById("id").value;
+
+  const response = await fetch("http://localhost:3000/api/v1/employee", {
+    method: "POST",
+    // NOTE: Content-Type is important
+    // it tells the server what type of data is being sent
+    // that way the server can parse the data correctly
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, name }),
+  });
+
+  const data = await response.json();
+  if (data.message) {
+    alert(data.message);
+    return;
+  }
+  alert(data.data.newEmployee.name + " has been added successfully");
+
+  fetchEmployees();
 }
 
 // TODO
-// add event listener to submit button
+async function deleteEmployee(id) {
+  if (!id) {
+    alert("Invalid id");
+  }
 
-// TODO
-// add event listener to delete button
+  const response = await fetch(`http://localhost:3000/api/v1/employee/${id}`, {
+    method: "DELETE",
+  });
 
-// TODO
-function createEmployee (){
-  // get data from input field
-  // send data to BE
-  // call fetchEmployees
+  const data = await response.json();
+  alert(data.data.deletedEmployee.name + " has been deleted successfully");
+
+  fetchEmployees();
 }
 
-// TODO
-function deleteEmployee (){
-  // get id
-  // send id to BE
-  // call fetchEmployees
-}
-
-fetchEmployees()
+fetchEmployees();
